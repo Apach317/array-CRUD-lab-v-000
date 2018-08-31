8a6651934b55031c7c4702992dedf7c3d30dfad6
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-31 04:31:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  nums = [1,2,3,4]
-  nums.unshift(0)
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
