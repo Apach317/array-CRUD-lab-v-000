083158b2ae7420b6e59e60591dd40a94cf499cb4
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-31 04:34:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  nums = [1,2,3,4]
-  nums.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  nums = [1,2,3,4]
-  nums.last
+  array.last
 end
